641d5ac264e6a3c86dc603db26dea66cd2d0c6e1
IsraWebDev <devisrael2024@outlook.com>
2024-09-02 20:40:29 -0600

    Números pares del 1 al 100

---
+++ NumerosParesDel1Al100.cs
@@ -0,0 +1,14 @@
+﻿
+//Numeros pares del 1 al 100 en orden acendente
+public class NumerosPares
+{
+    public static void Main()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            if ((i % 2 == 0))
+          
+                Console.Write(" {0} ", i); 
+        }
+    }
+}
